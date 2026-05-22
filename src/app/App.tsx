c945d92031b26ed4ff3314879226ee6d80ebeebe
Yash Kumar Singh <yashsingh1610@gmail.com>
2026-05-22 11:40:34 +0530

    ms clarity id

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import Clarity from "@microsoft/clarity";
 export default function App() {
 
   useEffect(() => {
-    Clarity.init("wuxtps2hvw");
+    Clarity.init("wuxtps2hvw"); // add clarity project ID here
   }, []);
 
   return <RouterProvider router={router} />;
